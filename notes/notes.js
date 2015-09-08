5df8ef6676aa8aac5e9ad5ac011fc0842c715a01
kennedy-computers <csclubkennedy@gmail.com>
2015-09-07 21:25:53 -0700

    Update notes.js

--- notes/notes.js
+++ notes/notes.js
@@ -28,7 +28,7 @@ $(function(){
 			.append("<img src='../images/"+val.toLowerCase()+".PNG'></iframe>")
 			.append("<div class='links'>"+val+"</div>")
 			.css({
-				"left": 8/window.innerWidth + (i%3)*31 +"%",
+				"left": "calc(8px + " + (i%3)*31 +"%)",
 				"top": 6+Math.floor(i/3)*(invRatio*30)+Math.floor(i/3)*1.5+"vw"
 			});
 	});//iterate through 'names'
