46aa674b7ad97a6b82eff25f2b32d4261d3155d2
useful-apps <kirtanushah@gmail.com>
2015-10-09 19:38:21 -0700

    Update notes.js

--- notes/notes.js
+++ notes/notes.js
@@ -32,7 +32,7 @@ $(function(){
 			var key = Object.keys(urls)[iname];
 			$("<div class='notesbox' name='"+key+"' id='notes"+iraw+"'></div>").appendTo("#notes")
 				.append("<img src='../images/"+key+"/"+val.toLowerCase()+".PNG'>")
-				.append("<span class='links'>"+val+"</span>")
+				.append($("<div class='links'></div>").append("<span>"+val+"</span>"))
 				.css({
 					"left": "calc(8px + " + (i%3)*31 +"%)",
 					"top": 6+Math.floor(i/3)*(invRatio*30)+Math.floor(i/3)+"vw"
