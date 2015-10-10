3edc76aff90f0d9b339c0e27f366d1bf92bb1861
useful-apps <kirtanushah@gmail.com>
2015-10-09 19:07:30 -0700

    Update notes.js

--- notes/notes.js
+++ notes/notes.js
@@ -5,15 +5,21 @@ $(function(){
 	var imgRatio = 1490/3200;
 	var names = [
 		[
+			"Setting up Java",
+			"Bare Minimum Code",
 			"The Print Statement",
-			"Conditionals"
-		]
+			"Conditionals",
+			"Variables and Datatypes"
+		]//arrays of names for each subject, object with properties not needed (like the urls object)
 	];//names of the notes
 	var urls = {
 		java: [
+			"https://docs.google.com/document/d/1n2vg4Zh_mxjoSgWo6vdeHzUcnUjGV1b2eys3u3o9o44/",
+			"https://docs.google.com/document/d/1Qb4OkBEGyo0LzwkP6AnsETOBdB1wpBTHEutcbNxrnko/",
 			"https://docs.google.com/document/d/1cwoA8YlNfaZ4PdfYTiRTrwqY4x8BMKV20iFh7swW4hM/",
-			"https://docs.google.com/document/d/1Mp3J0f4ayPhZ1J4pUHU0KhzFZYgSfcg6XTWMsgOxJyE/"
-		]
+			"https://docs.google.com/document/d/1Mp3J0f4ayPhZ1J4pUHU0KhzFZYgSfcg6XTWMsgOxJyE/",
+			"https://docs.google.com/document/d/1zmaiXYm-iQD3DJfuyitXVO64pXnlhW03YHRNcLhzPTE/"
+		] //add each subject as a new property of object urls
 	};//notes will be on google docs.
 	var top;
 	//java notes
@@ -29,13 +35,17 @@ $(function(){
 					"left": "calc(8px + " + (i%3)*31 +"%)",
 					"top": 6+Math.floor(i/3)*(invRatio*30)+Math.floor(i/3)+"vw"
 				});
-		});//iterate through 'names'
-	}
+		});
+	}//iterate through urls object and subarray to create note boxes
 	
-	$(".notesbox").click(function(){
+	$(".notesbox").click(function(){ //click event to go to url of notes when notesbox clicked
 		window.open(urls[this.getAttribute("name")][parseInt(this.id.replace("notes",""))]);//open doc on click
+		//window.open = opens the document
+		//urls[this.getAttribute("name")] is for getting a property of object, accessing it by string
+		//last [] specifies which element of array to retrieve
 	}).height(imgRatio*0.3*window.innerWidth);
-	var $nb = $(".notesbox").eq($(".notesbox").length-1);
+	//set height of each notesbox
+	var $nb = $(".notesbox").eq($(".notesbox").length-1);//get the last notesbox element
 	$("h1").eq(1).css("top",$nb.offset().top+$nb.height()+"px").css("left","8px")
-	
+	//get the last heading and set it to under the last notesbox
 });
